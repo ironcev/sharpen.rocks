06e401c786e3f87e44fdcdfeb89f38416813afce
Igor Rončević <ironcev@hotmail.com>
2017-11-25 22:08:37 +0100

    feat: Redirects all requests to GitHub

---
+++ SharpenRocks/Controllers/RedirectController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace SharpenRocks.Controllers
+{
+    public class RedirectController : Controller
+    {
+        public ActionResult Index()
+        {
+	        return Redirect("https://github.com/ironcev/Sharpen");
+        }
+    }
+}
